6848bb5f3316bb4ddd2c4d12436c15276f7ff6f3
Alberto Taiuti <alberto.taiuti@gmail.com>
2017-01-27 13:06:50 +0000

    Add base unit test for the ctor of the stream class.
    Add a basic unit test which checks for the exceptions thrown
    by the ctor of the stream class.

--- unit_tests/test_0.cpp
+++ unit_tests/test_0.cpp
@@ -5,56 +5,61 @@
 #include <fstream>
 
 
-TEST_CASE("Factorials are computed", "[factorial]") {
+TEST_CASE("spv utils is tested with correct spir-v binary",
+          "[spv-utils-correct-spvbin]") {
 
   // Read spv binary module from a file
-  std::ifstream spv_file("../sample_spv_modules/test.frag.spv", std::ios::binary |
+  std::ifstream spv_file("./sample_spv_modules/test.frag.spv", std::ios::binary |
                          std::ios::ate | std::ios::in);
   REQUIRE(spv_file.is_open() == true);
   std::streampos size = spv_file.tellg();
+  CHECK(size > 0);
 
   spv_file.seekg(0, std::ios::beg);
   char *data = new char[size];
   spv_file.read(data, size);
   spv_file.close();
 
-  // Parse the module
+
   SECTION("Passing a null ptr for the data throws an exception") {
+    REQUIRE_THROWS_AS(sut::OpcodeStream(nullptr, size),
+                      sut::InvalidParameter);
+  }
+  SECTION("Passing size zero for the size throws an exception") {
+    REQUIRE_THROWS_AS(sut::OpcodeStream(data, 0),
+                      sut::InvalidParameter);
+  }
+  SECTION("Passing size zero for the size and nullptr throws an exception") {
+    REQUIRE_THROWS_AS(sut::OpcodeStream(nullptr, 0),
+                      sut::InvalidParameter);
+  }
+  SECTION("Passing correct data ptr and size creates the object") {
+    REQUIRE_NOTHROW(sut::OpcodeStream(data, size));
+
     sut::OpcodeStream stream(data, size);
 
+    for (auto &i : stream) {
+      if (i.GetOpcode() == spv::Op::OpCapability) {
+        uint32_t instruction = 0xDEADBEEF;
+        std::vector<uint32_t> longer_instruction = {
+          0xDEADBEEF,
+          0xDEADBEEF,
+          0xDEADBEEF,
+          0xDEADBEEF
+        };
+        std::vector<uint32_t> longer_instruction_2 = {
+          0x1EADBEEF,
+          0x1EADBEEF,
+          0x1EADBEEF,
+          0x1EADBEEF
+        };
+        i.InsertBefore(longer_instruction.data(), longer_instruction.size());
+        i.InsertAfter(&instruction, 1U);
+        i.InsertAfter(longer_instruction.data(), longer_instruction.size());
+        i.InsertAfter(longer_instruction.data(), longer_instruction.size());
+        i.InsertBefore(longer_instruction_2.data(), longer_instruction_2.size());
+        i.Remove();
+      }
+    }
   }
-  //for (auto &i : stream) {
-  //  if (i.GetOpcode() == spv::Op::OpCapability) {
-  //    uint32_t instruction = 0xDEADBEEF;
-  //    std::vector<uint32_t> longer_instruction = {
-  //      0xDEADBEEF,
-  //      0xDEADBEEF,
-  //      0xDEADBEEF,
-  //      0xDEADBEEF
-  //    };
-  //    std::vector<uint32_t> longer_instruction_2 = {
-  //      0x1EADBEEF,
-  //      0x1EADBEEF,
-  //      0x1EADBEEF,
-  //      0x1EADBEEF
-  //    };
-  //    i.InsertBefore(longer_instruction.data(), longer_instruction.size());
-  //    i.InsertAfter(&instruction, 1U);
-  //    i.InsertAfter(longer_instruction.data(), longer_instruction.size());
-  //    i.InsertAfter(longer_instruction.data(), longer_instruction.size());
-  //    i.InsertBefore(longer_instruction_2.data(), longer_instruction_2.size());
-  //    i.Remove();
-  //  }
-  //}
-
-  //std::vector<uint32_t> filtered_stream = stream.EmitFilteredStream();
-
-  //  delete[] data;
-  //  data = nullptr;
-  //}
-  //REQUIRE(Factorial(0) == 1);
-  //REQUIRE(Factorial(1) == 1);
-  //REQUIRE(Factorial(2) == 2);
-  //REQUIRE(Factorial(3) == 6);
-  //REQUIRE(Factorial(10) == 3628800);
 }
